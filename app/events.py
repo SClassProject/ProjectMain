11ab8261dd44068d5ec8585f73c26b5b037899bf
unaif53 <jyckss12@ewhain.net>
2022-11-15 17:59:55 +0900

    recognition

--- app/events.py
+++ app/events.py
@@ -1,5 +1,29 @@
 from flask import session
 from flask_socketio import emit #, Namespace, join_room, leave_room, close_room, rooms, connect, disconnect
+from time import time
+import schedule
+
+socketTime = {}
+state = {}
+pre_state= {}
+index = 0
+
+def checkAttendence():
+    now = time()
+    for key in socketTime:
+        pre_state[key] == state[key] # 기존 상태를 이전 상태로 저장
+        if state[key]=="on":
+            if now-socketTime[key] > 300: # 현재시간부터 소켓 받은 시간까지 차이가 5분 초과면 자리비움
+                state[key]=="off"
+        elif state[key]=="off":
+            if now-socketTime[key] < 300: # 현재시간부터 소켓 받은 시간까지 차이가 5분 미만이면 다시 왔음
+                state[key]=="on"
+    threading.Timer(300, checkAttendence).start()
+
+schedule.every(5).minutes.do(checkAttendence) # 5분마다 state 검사
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 def socketio_init(socketio):
     @socketio.on('join', namespace='/room')
@@ -10,11 +34,24 @@ def socketio_init(socketio):
     
     @socketio.on('attend', namespace='/room')
     def attend(message) :
-        print(message['msg'])
+        # print(message['msg'])
+        id = message['id']
+        socketTime[id] = time() #socket 전송받은 시간 저장
+        if index == 0: #초기화
+            state[id] = "on"
+            pre_state[id] = "on"
+            index = 1
+
+        if state[id] != pre_state[id]: # 상태 변화되면 emit
+            if state[id]=="on":
+                socketio.emit('onalert', id)
+            else state[id]=="off":
+                socketio.emit('offalert',id)
 
     @socketio.on('hand', namespace='/room')
     def raiseHand(message) :
-        print(message['msg'])
+        # print(message['msg'])
+        socketio.emit('handalert', message['msg'])
 
     @socketio.on('leave', namespace='/room')
     def leave(message) :
